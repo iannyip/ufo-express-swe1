c65d5589928524968bd186989d6b568cf2627daf
iannyip <ianyip@nullspace.com.sg>
2021-02-10 12:29:41 +0800

    all routes created. PUT and DELETE requests not functional

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // import modules
 import express from 'express';
-import {add, read} from './jsonFileStorage.js';
+import {add, read, write} from './jsonFileStorage.js';
 
 // Setup
 const PORT = 3004;
@@ -47,8 +47,70 @@ app.get("/sighting/:index/edit", (request, response) => {
 
   read("data.json", (data, error) => {
     const sightingObj = data.sightings[index];
+    sightingObj["index"] = index;
     console.log(sightingObj);
     response.render("sighting-edit", {sightingObj});
+  })
+})
+
+// Needs help
+app.put("/sighting/:index/edit", (request, response) => {
+  console.log('banana');
+  const index = request.params.index;
+  read("data.json", (data, error) => {
+    data.sightings[index] = request.body;
+    write("data.json", data, (doneData, error) => {
+      response.redirect("/");
+    })
+  })
+})
+
+// Needs help
+app.get("/sighting/:index/delete", (request, response) => {
+  console.log('entered delete route');
+  const index = request.params.index;
+  console.log(index);
+  read("data.json", (data, error) => {
+    data.sightings.splice(index, 1);
+    write("data.json", data, (donedata, error) => {
+      response.redirect("/");
+    })
+  })
+})
+
+app.get("/shapes", (request, response) => {
+  console.log('getting shapes list');
+  const shapesObj = {};
+  read("data.json", (data, error) => {
+    data.sightings.forEach(sight => {
+      console.log(sight.shape);
+      if (sight.shape in shapesObj){
+        shapesObj[sight.shape] += 1;
+      }
+      else{
+        shapesObj[sight.shape] = 1;
+      }
+    })
+    console.log(shapesObj);
+    // response.send('yay');
+    response.render("shapes", {shapesObj});
+  })
+})
+
+app.get("/shapes/:shape", (request, response) => {
+  const shape = request.params.shape;
+  console.log(shape);
+  const shapeObjArr = [];
+  read("data.json", (data, error) => {
+    data.sightings.forEach(sight => {
+      if (sight.shape === shape){
+        shapeObjArr.push(sight);
+      }
+    })
+    console.log(shapeObjArr);
+    // response.send("yay");
+    response.render("shapes-result", {shapeObjArr});
+
   })
 })
 
