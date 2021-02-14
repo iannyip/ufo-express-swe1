84f4f7dee3bbd8c6a0a57f188f8ec853bd8fba3c
iannyip <ianyip@nullspace.com.sg>
2021-02-15 01:30:39 +0800

    sort, date created. to fix validation formatting

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // import modules
 import express from 'express';
 import {add, read, write} from './jsonFileStorage.js';
+import methodOverride from 'method-override';
 
 // Setup
 const PORT = 3004;
@@ -8,6 +9,19 @@ const app = express();
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public")); // Allows you to use external stylesheets in "public" (or custom folder name)
+app.use(methodOverride('_method'));
+let sortStatus = true;
+let sortCity = true;
+
+// Define helper function
+const validateEmptyFields = (res, objToValidate, index) => {
+  for (const property in objToValidate){
+    if (objToValidate[property] === ""){
+      console.log(`${property} is blank!!`);
+      res.redirect(`/sighting/${index}/edit`);
+    }
+  }
+}
 
 // Define routes/ middleware 
 app.get('/sighting', (request, response) => {
@@ -16,12 +30,15 @@ app.get('/sighting', (request, response) => {
 
 app.post('/sighting', (request, res) => {
   const newData = request.body;
+  
+  console.log("newData: ", newData);
+  
   add('data.json', "sightings", newData, (data, error) =>{
     if (error){
       console.log("error",error);
     }
     const newIndex = data.sightings.length - 1;
-    const sightingObj = data.sightings[newIndex]
+    validateEmptyFields(res, newData, newIndex);
     res.redirect(`/sighting/${newIndex}`);
   });
 })
@@ -37,7 +54,33 @@ app.get('/sighting/:index', (request, response) => {
 
 app.get('/', (request, response) => {
   console.log('incoming request');
+  console.log("sort Index is: ", request.query.sortIndex)
+  console.log("sort City is: ", request.query.sortCity)
   read('data.json', (data, error) => {
+    for (let i = 0; i < data.sightings.length; i += 1){
+      data.sightings[i].index = i+1;
+    }
+    if(request.query.sortIndex==="true" && sortStatus === true){
+      data.sightings.reverse();
+      sortStatus = false;
+    } else{
+      sortStatus = true;
+    }
+    if (request.query.sortCity ==="true" && sortCity === true){
+      data.sightings.sort((obj1, obj2) =>{
+        if (obj1.city > obj2.city) return 1;
+        if (obj1.city < obj2.city) return -1;
+        return 0;
+      })
+      sortCity = false;
+    } else if (request.query.sortCity ==="true" && sortCity === false) {
+      data.sightings.sort((obj1, obj2) =>{
+        if (obj1.city < obj2.city) return 1;
+        if (obj1.city > obj2.city) return -1;
+        return 0;
+      })
+      sortCity = true;
+    }
     response.render('main', data);
   })
 })
@@ -53,7 +96,6 @@ app.get("/sighting/:index/edit", (request, response) => {
   })
 })
 
-// Needs help
 app.put("/sighting/:index/edit", (request, response) => {
   console.log('banana');
   const index = request.params.index;
@@ -65,8 +107,7 @@ app.put("/sighting/:index/edit", (request, response) => {
   })
 })
 
-// Needs help
-app.get("/sighting/:index/delete", (request, response) => {
+app.delete("/sighting/:index/delete", (request, response) => {
   console.log('entered delete route');
   const index = request.params.index;
   console.log(index);
